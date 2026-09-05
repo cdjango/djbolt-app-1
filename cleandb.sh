f89d2e6d4638ae62af653f138687ca7611c7e9f4
VueDJ <none@none.com>
2026-09-05 12:54:34 +0530

    Minor fix

--- cleandb.sh
+++ cleandb.sh
@@ -3,7 +3,7 @@ find . -path "*/migrations/*.py" -not -path "*/.*" -not -name "__init__.py" -del
 find . -type d -name "__pycache__" -exec rm -rf {} +
 echo "Database deleted. Press any key to continue making fresh database and migrations..."
 read -n 1 -s
-uv run manage.py makemigrations core
+uv run manage.py makemigrations auth_app core
 uv run manage.py migrate
 uv run manage.py createsuperuser --email admin@admin.com
 uv run manage.py runbolt --dev
